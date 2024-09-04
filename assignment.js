2e9876f4ffd724201938dc7258707bd7ccee7bdc
Halim <book1001.db@gmail.com>
2024-09-04 12:39:37 -0400

    update

---
+++ assignment.js
@@ -0,0 +1,9 @@
+const textWithLineBreaks = `
+  ASSIGNMENT
+  
+  ~ 9/4: Please submit the 1st day questionnaire form by today!
+  ~ 9/11: Prepare to share your workshop results!
+`;
+
+const contentDiv = document.getElementById('assignment');
+contentDiv.innerText = textWithLineBreaks;
