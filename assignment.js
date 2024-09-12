d73f5c62d0784ed5b2f1300efa3dc7371a0bd1f5
Halim <book1001.db@gmail.com>
2024-09-11 20:33:20 -0400

    upload

--- assignment.js
+++ assignment.js
@@ -1,9 +1,18 @@
 const textWithLineBreaks = `
   ASSIGNMENT
+
+  ~ 9/18: Prepare to share your workshop results + Bring your mouse!
+  ~ 9/18: The first draft of your "World Wild Wonderland" project
   
-  ~ 9/4: Please submit the 1st day questionnaire form by today!
-  ~ 9/11: Prepare to share your workshop results + Bring your mouse!
 `;
 
 const contentDiv = document.getElementById('assignment');
 contentDiv.innerText = textWithLineBreaks;
+
+
+// const textWithLineBreaks = `
+//   ASSIGNMENT
+
+//   ~ 9/4: Please submit the 1st day questionnaire form by today!
+//   ~ 9/11: Prepare to share your workshop results + Bring your mouse!
+// `;
