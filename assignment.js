0afa5d58ca7f3aa3d93b7e000b549f10a293627f
Halim <book1001.db@gmail.com>
2024-09-25 18:31:00 -0400

    assignment

--- assignment.js
+++ assignment.js
@@ -1,10 +1,9 @@
 const textWithLineBreaks = `
   ASSIGNMENT
 
-  ~ 9/25: There will be no Brief Sharing in the class on the 25th. Instead, we will have small group discussions and 1:1 meetings to review the 2nd draft of your World Wild Wonderland project.
+  ~ 10/2: World Wild Wonderland) We will briefly share your progress (no need to prepare slides)
+  ~ 10/2: World Wild Wonderland) Prepare for 1:1 meetings
 
-  ~ 9/25: Bring your mouse! (We will learn other features of Spline on the 25th.)
-  
 `;
 
 const contentDiv = document.getElementById('assignment');
@@ -17,6 +16,9 @@ contentDiv.innerText = textWithLineBreaks;
 // ~ 9/18: Prepare to share your workshop results + Bring your mouse!
 // ~ 9/18: The first draft of your "World Wild Wonderland" project
 
-//   ~ 9/4: Please submit the 1st day questionnaire form by today!
-//   ~ 9/11: Prepare to share your workshop results + Bring your mouse!
+// ~ 9/4: Please submit the 1st day questionnaire form by today!
+// ~ 9/11: Prepare to share your workshop results + Bring your mouse!
+
+// ~ 9/25: There will be no Brief Sharing in the class on the 25th. Instead, we will have small group discussions and 1:1 meetings to review the 2nd draft of your World Wild Wonderland project.
+// ~ 9/25: Bring your mouse! (We will learn other features of Spline on the 25th.)
 // `;
